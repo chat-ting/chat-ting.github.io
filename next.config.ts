930132148578fde7bade6591568f0c0a4ab9f255
vvyre <seung.yoon.yu@gmail.com>
2025-04-01 18:42:51 +0900

    chore: 빌드 스크립트 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   assetPrefix: process.env.NODE_ENV === 'production' ? 'https://chat-ting.github.io/chat-ting.github.io' : '',
-};
+  output: 'export',
+  distDir: 'out',
+}
 
-export default nextConfig;
+export default nextConfig
