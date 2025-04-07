87a0b947fb9008c7f7b28c01d9e26cc9c244e62f
vvyre <seung.yoon.yu@gmail.com>
2025-04-07 15:06:42 +0900

    feat: 메인 페이지 샘플 작성

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,37 @@ import { titles } from '~/_lib/util/read-writings'
 
 export default async function Page() {
   return (
-    <ul>
-      {titles.map(title => (
-        <li key={title}>
-          <Link href={`/${title}`}>{title}</Link>
+    <div>
+      <h1>CHAT-TING</h1>
+      <ul>
+        <h2>레포지토리</h2>
+        <li>
+          <Link href="https://github.com/chat-ting/client.app" target="_blank">
+            frontend application
+          </Link>
         </li>
-      ))}
-    </ul>
+        <li>
+          <Link href="https://github.com/chat-ting/chat" target="_blank">
+            backend (chat)
+          </Link>
+        </li>
+        <li>
+          <Link href="https://github.com/chat-ting/member" target="_blank">
+            backend (member)
+          </Link>
+        </li>
+        <li>
+          <Link href="https://github.com/chat-ting/member" target="_blank">
+            backend (member)
+          </Link>
+        </li>
+        <h2>기술 문서</h2>
+        {titles.map(title => (
+          <li key={title}>
+            <Link href={`/${title}`}>{title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
