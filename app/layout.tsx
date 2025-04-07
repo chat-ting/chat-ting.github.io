b3ec17be93489895535751fd933de004998cbc9e
vvyre <seung.yoon.yu@gmail.com>
2025-04-07 15:30:03 +0900

    docs: 메타데이터 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { setGlobalStyle } from '~/_lib/global-style.css'
 import { getCssText } from '~/stitches.config'
 
 export const metadata: Metadata = {
-  title: 'Writings by JadeWyre',
-  description: 'Writings by JadeWyre',
+  title: 'chat-ting',
+  description: 'chat-ting 기술 문서',
 }
 
 export default function RootLayout({
