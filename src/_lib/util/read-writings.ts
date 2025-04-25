6281644459013f477bae2b37f4b35c7fb57f615c
vvyre <seung.yoon.yu@gmail.com>
2025-04-25 15:41:13 +0900

    top level async를 브라우저 환경에서 지원하지 않아 생기는 문제 해결

--- src/_lib/util/read-writings.ts
+++ src/_lib/util/read-writings.ts
@@ -1,22 +1,28 @@
 export const getUrl = (title: string) =>
   `https://raw.githubusercontent.com/chat-ting/chat-ting.github.io/refs/heads/main/__writings/${encodeURIComponent(
     title
-  )}`
+  )}`;
 
-const response = await fetch(
-  'https://raw.githubusercontent.com/chat-ting/chat-ting.github.io/refs/heads/main/__writings/writings.map.json'
-)
+const res = async () => {
+  return await fetch(
+    'https://raw.githubusercontent.com/chat-ting/chat-ting.github.io/refs/heads/main/__writings/writings.map.json'
+  );
+};
 
-export const writingsWithFileType = (await response.json()) as string[]
-
-export const titles: string[] = []
+export const writingsWithFileType = async () => {
+  const response = await res();
+  return (await response.json()) as Promise<string[]>;
+};
+export const titles: string[] = [];
 
 //returns "xxxx-xxxx-xxxx.md"
-export const writingsData = writingsWithFileType.map(file => {
-  const DOT = file.indexOf('.')
-  const TITLE = file.slice(0, DOT)
-  titles.push(TITLE)
-  return {
-    [TITLE]: getUrl(file),
-  }
-})
+export const writingsData = writingsWithFileType().then(data =>
+  data.map(file => {
+    const DOT = file.indexOf('.');
+    const TITLE = file.slice(0, DOT);
+    titles.push(TITLE);
+    return {
+      [TITLE]: getUrl(file),
+    };
+  })
+);
